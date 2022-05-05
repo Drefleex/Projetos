5a349c30048de2bf83fbbe926fe089066bd9a25e
André Siqueira <dre.fleex@gmail.com>
2022-05-06 00:15:10 +0100

    Adicionando projetos

---
+++ Pokemon RPG PYTHON/pokemon.py
@@ -0,0 +1,62 @@
+import random
+
+class Pokemon:
+    def __init__(self, especie, level=None, nome=None):
+        self.especie = especie
+        
+        
+        if level:
+           self.level = level
+        else:
+            self.level = random.randint(1,100)
+
+        if nome:
+            self.nome = nome
+        else:
+            self.nome = especie
+    
+        self.ataque = self.level * 5
+        self.vida = self.level * 10
+
+    def __str__(self):
+        return "{}({})".format(self.nome, self.level)
+
+    def atacar(self, pokemon):
+        atq_efetivo = int((self.ataque * random.random() * 1.3))
+        pokemon.vida -= atq_efetivo
+        print("{} perdeu {} pontos de vida".format(pokemon, atq_efetivo))
+        
+        if pokemon.vida <= 0:
+            print("{} foi derrotado".format(pokemon))
+            return True
+        
+        else:
+            return False
+
+
+class PokemonEletrico(Pokemon):
+    tipo = "Eletrico"
+    
+    def atacar(self, pokemon):
+        print("{} lançou um raio do trovão em {}".format(self, pokemon))
+        return super().atacar(pokemon)
+
+class PokemonFogo(Pokemon):
+    tipo = "Fogo"
+    
+    def atacar(self, pokemon):
+        print("{} lançou uma bola de fogo na cabeça de {}".format(self, pokemon))
+        return super().atacar(pokemon)
+
+class PokemonAgua(Pokemon):
+    tipo = "Agua"
+    
+    def atacar(self, pokemon):
+        print("{} lançou um jato d'água na cabeça de {}".format(self, pokemon))
+        return super().atacar(pokemon)
+    
+
+        
+        
+   
+
